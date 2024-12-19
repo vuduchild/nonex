ac8dc55098812de79b3653d9e649242ccdbde5ea
Roy Rothenberg <royrothenberg@gmail.com>
2024-12-18 21:34:14 -0500

    initial commit

---
+++ nonex/__init__.py
@@ -0,0 +1,27 @@
+from typing import Callable, Iterable, TypeVar
+
+T = TypeVar("T")
+
+
+def nonethrows(value: T | None, msg: str | None = None) -> T:
+    """Coerce an value to non-optional. Raise TypeError if the value is None"""
+    if value is None:
+        raise TypeError(msg or "Unexpected None")
+    return value
+
+
+def first(
+    iterable: Iterable[T], filter_func: Callable[[T], bool] | None = None
+) -> T | None:
+    """Get the first element from an iterable. Returns None if empty"""
+    filtered = filter(lambda x: filter_func(x) if filter_func else True, iterable)
+    try:
+        return next(filtered)
+    except StopIteration:
+        return None
+
+
+def firstx(iterable: Iterable[T], filter_func: Callable[[T], bool] | None = None) -> T:
+    """Get the first element from an iterable. Raises TypeError if empty"""
+    return nonethrows(first(iterable, filter_func))
+
